99e295886ef8d9ccb3f6cc2f371d7d93daca1c84
Robert Summers <rsummers02@gmail.com>
2016-08-02 12:59:31 +0100

    Javabuzz first commit

---
+++ spec/javabuzzSpec.js
@@ -0,0 +1,43 @@
+describe('Javzbuzz', function(){
+
+  var javabuzz;
+
+  describe('knows when a number is',function() {
+
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+    it('divisible by 5',function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by 15',function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is not',function(){
+
+    it('divisible by 3', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5',function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+    it('divisible by 15',function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false)
+    });
+
+  });
+
+});
