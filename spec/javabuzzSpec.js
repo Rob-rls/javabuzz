ce066797546333ec176331aa8f1fd6060e74201c
Robert Summers <rsummers02@gmail.com>
2016-08-02 13:39:56 +0100

    Javabuzz 01!

--- spec/javabuzzSpec.js
+++ spec/javabuzzSpec.js
@@ -2,20 +2,21 @@ describe('Javzbuzz', function(){
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is',function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     it('divisible by 5',function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFive(5)).toBe(true);
     });
 
     it('divisible by 15',function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
@@ -24,18 +25,35 @@ describe('Javzbuzz', function(){
   describe('knows when a number is not',function(){
 
     it('divisible by 3', function(){
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
 
     it('divisible by 5',function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFive(1)).toBe(false);
     });
 
     it('divisible by 15',function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFifteen(1)).toBe(false)
+    });
+
+  });
+
+  describe('when playing, says',function() {
+
+    it('"Java" when a number is divisible by 3', function(){
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(javabuzz.says(5)).toEqual("Buzz");
+    });
+
+    it('"JavaBuzz" when a number is divisible by 5', function() {
+      expect(javabuzz.says(15)).toEqual("JavaBuzz");
+    });
+
+    it('number when it isn\'t any of the above', function () {
+      expect(javabuzz.says(2)).toEqual(2);
     });
 
   });
